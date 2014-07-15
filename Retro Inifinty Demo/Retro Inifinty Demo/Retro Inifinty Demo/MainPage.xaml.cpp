5097c2f8b112ac2470e1903cec3f0ae668812004
spandey-bd <Sudheesh.Nair@LT1ATEMP003.ushustech.com>
2014-07-15 12:13:25 +0530

    New files added

---
+++ Retro Inifinty Demo/Retro Inifinty Demo/Retro Inifinty Demo/MainPage.xaml.cpp
@@ -0,0 +1,50 @@
+﻿//
+// MainPage.xaml.cpp
+// Implementation of the MainPage class.
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+using namespace Retro_Inifinty_Demo;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+using namespace Windows::UI::Xaml::Media::Animation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+	gameLogoStoryboard_->Begin();
+	cloudStoryboard_->Begin();
+}
+
+
+
+/// <summary>
+/// Invoked when this page is about to be displayed in a Frame.
+/// </summary>
+/// <param name="e">Event data that describes how this page was reached.  The Parameter
+/// property is typically used to configure the page.</param>
+void MainPage::OnNavigatedTo(NavigationEventArgs^ e)
+{
+	(void) e;	// Unused parameter
+}
+
+
+void Retro_Inifinty_Demo::MainPage::Button_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	
+	//CocosDenshion::SimpleAudioEngine::sharedEngine()->//("pew-pew-lei.wav");
+	btnClickStoryboard_->Begin();
+	
+}
